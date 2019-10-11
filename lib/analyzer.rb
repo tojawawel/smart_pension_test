4c789df21f635833eeaf73fb60084609dca403c6
Bartosz Gawel <bgawel93@gmail.com>
2019-10-11 14:06:09 +0200

    add unique method and specs for empty and correct file

--- lib/analyzer.rb
+++ lib/analyzer.rb
@@ -16,6 +16,15 @@ class Analyzer
     all_views.sort_by { |_k, v| -v }.to_h
   end
 
+  def unique_views
+    return 'No logs inside' if empty_logs?
+
+    unique_views = {}
+
+    @array_of_logs.uniq.each { |log| unique_views[log[0]] = unique_views[log[0]].to_i + 1 }
+    unique_views.sort_by { |_k, v| -v }.to_h
+  end
+
   private
 
   def empty_logs?
--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -23,4 +23,22 @@ describe Analyzer do
       end
     end
   end
+
+  describe '.unique views' do
+    let(:result) { { 'home' => 4, 'about' => 2, 'info' => 2 } }
+
+    context 'when logfile is empty' do
+      let(:logfile) { 'spec/empty.log' }
+
+      it 'returns a message' do
+        expect(subject.unique_views).to eq('No logs inside')
+      end
+    end
+
+    context 'when logfile is not empty' do
+      it 'returns the expected result' do
+        expect(subject.unique_views).to eq(result)
+      end
+    end
+  end
 end
