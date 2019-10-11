ecffdcec9b8bc21b5a96db4636e6224d59858d3c
Bartosz Gawel <bgawel93@gmail.com>
2019-10-11 13:17:20 +0200

    add specs for parse method with correct file

--- lib/parser.rb
+++ lib/parser.rb
@@ -3,6 +3,10 @@
 class Parser
   def parse(file)
     raise 'No such file' unless file_exists?(file)
+
+    array_of_logs = []
+    File.open(file).each { |line| array_of_logs << line.split(' ') }
+    array_of_logs
   end
 
   private
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -8,11 +8,34 @@ describe Parser do
   let(:logfile) { 'spec/test.log' }
 
   describe '.parse' do
+    let(:result) do
+      [['home', '111.111.111.111'],
+       ['home', '111.111.111.111'],
+       ['home', '222.222.222.222'],
+       ['home', '333.333.333.333'],
+       ['home', '333.333.333.333'],
+       ['home', '444.444.444.444'],
+       ['about', '333.333.333.333'],
+       ['about', '111.111.111.111'],
+       ['info', '555.555.555.555'],
+       ['info', '555.555.555.555'],
+       ['info', '111.111.111.111']]
+    end
     context 'when file is incorrect' do
       let(:logfile) { 'spec/nonexist.log' }
 
       it 'raises an error' do
         expect { subject.parse(logfile) }.to raise_error(RuntimeError, 'No such file')
+      end
+    end
+
+    context 'when file is correct' do
+      it 'parses the file' do
+        expect(subject.parse(logfile)).to eq(result)
+      end
+
+      it 'result is instance of array' do
+        expect(subject.parse(logfile)).to be_instance_of(Array)
       end
     end
   end
